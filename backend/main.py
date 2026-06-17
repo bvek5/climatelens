097a03be31f9c36ffebc7512cfbd866030d42525
bmalla5 <your@email.com>
2026-06-17 19:49:18 -0400

    Add latest country climate data endpoint

--- backend/main.py
+++ backend/main.py
@@ -23,6 +23,35 @@ def get_countries():
         "countries": countries
     }
 
+
+@app.get("/country/{country_name}/latest")
+def get_latest_country_data(country_name: str):
+
+    df = pd.read_csv("../data/owid-co2-data.csv")
+
+    result = df[
+        df["country"].str.lower() == country_name.lower()
+    ]
+
+    if result.empty:
+        return {"error": "No data found"}
+
+    result = result.sort_values("year", ascending=False)
+
+    row = result.iloc[0].where(pd.notnull(result.iloc[0]), None)
+
+    return {
+        "country": row["country"],
+        "latest_year": int(row["year"]),
+        "iso_code": row["iso_code"],
+        "population": row["population"],
+        "gdp": row["gdp"],
+        "co2": row["co2"],
+        "co2_per_capita": row["co2_per_capita"],
+        "total_ghg": row["total_ghg"],
+        "primary_energy_consumption": row["primary_energy_consumption"]
+    }
+
 @app.get("/country/{country_name}/history")
 def get_country_history(country_name: str):
 
