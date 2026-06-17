3a649d36e40294435cc205c3ec9012d37669604e
bmalla5 <your@email.com>
2026-06-17 19:20:42 -0400

    Connect ClimateLens to real OWID climate data

--- backend/main.py
+++ backend/main.py
@@ -10,12 +10,10 @@ def home():
     "status": "Running"
 }
  
-
-
 @app.get("/country/{country_name}/{year}")
 def get_country_data(country_name: str, year: int):
 
-    df = pd.read_csv("../data/climate_data.csv")
+    df = pd.read_csv("../data/owid-co2-data.csv")
 
     result = df[
         (df["country"].str.lower() == country_name.lower()) &
@@ -25,4 +23,18 @@ def get_country_data(country_name: str, year: int):
     if result.empty:
         return {"error": "No data found"}
 
-    return result.iloc[0].to_dict()
+    row = result.iloc[0].where(pd.notnull(result.iloc[0]), None)
+    return {
+    "country": row["country"],
+    "year": int(row["year"]),
+    "iso_code": row["iso_code"],
+    "population": row["population"],
+    "gdp": row["gdp"],
+    "co2": row["co2"],
+    "co2_per_capita": row["co2_per_capita"],
+    "co2_growth_percent": row["co2_growth_prct"],
+    "energy_per_capita": row["energy_per_capita"],
+    "primary_energy_consumption": row["primary_energy_consumption"],
+    "total_ghg": row["total_ghg"],
+    "temperature_change_from_co2": row["temperature_change_from_co2"]
+}
