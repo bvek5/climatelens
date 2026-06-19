47dc96ed1b1f8a1193d2b0df3f0868e54a20b366
bmalla5 <your@email.com>
2026-06-19 01:19:54 -0400

    Allow Vercel frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,11 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://climatelens-topaz.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
